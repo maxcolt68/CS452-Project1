45cf1ddc49b554f076d14202a5f85a36ded3c27b
max barnaby <barnabmc@clarkson.edu>
2022-03-08 11:21:23 -0500

    Jumping cube

--- p1.js
+++ p1.js
@@ -1,26 +1,65 @@
-let app = new PIXI.Application({width: 640, height: 360});
+let plane = "floor";
+let yCubeMovement = 0;
+let yCubeTarget = 0;
+
+let app = new PIXI.Application({width: 1280, height: 720, backgroundColor: 0x403E3E});
 document.body.appendChild(app.view);
 
-let sprite = PIXI.Sprite.from("assets/ball.png");
+let ceiling = new PIXI.Graphics();
+let floor = new PIXI.Graphics();
 
-app.stage.addChild(sprite);
+const cube = new PIXI.Container();
 
-let time = 0.0;
+let cubeFill = new PIXI.Graphics();
+let cubeOutline = new PIXI.Graphics();
 
-app.ticker.add((delta) =>{
-	
-	time += delta;
-	
-	sprite.x = 100 + Math.cos(time/50.0) * 100;
-});
+ceiling.beginFill(0x0B7CDF);
+ceiling.drawRect(0, 0, 1280, 75);
+ceiling.endFill();
+
+floor.beginFill(0xDF0B15);
+floor.drawRect(0, 720-75, 1280, 75);
+floor.endFill();
+
+cubeFill.beginFill(0x78E73A);
+cubeFill.drawRect(13, 720-75-77, 75, 75);
+cubeFill.endFill();
+
+cubeOutline.beginFill(0x000000);
+cubeOutline.drawRect(10, 720-75-80, 81, 81);
+cubeOutline.endFill();
+
+
+cube.addChild(cubeOutline);
+cube.addChild(cubeFill);
+
+
+app.stage.addChild(ceiling);
+app.stage.addChild(floor);
+app.stage.addChild(cube);
+
+
+ app.ticker.add((delta) =>{
+	 if(cube.y != yCubeTarget){
+	 		cube.setTransform(cube.x, cube.y + yCubeMovement);
+		}
+ });
+
 
 function buttonHandler(event){
-	
+
 	let key = event.key;
-	
+	console.log(key)
 	if(key == " "){
-		alert("Space was pressed")
+		if(plane == "floor"){
+			plane = "ceiling";
+			yCubeTarget = -490;
+			yCubeMovement = -5;
+		}
+		else if(plane == "ceiling"){
+			plane = "floor";
+			yCubeTarget = 0;
+			yCubeMovement = 5;
+		}
 	}
-	
-	
 }
