5898028160f608579f97e1b4cb322ba486ca1677
max barnaby <barnabmc@clarkson.edu>
2022-03-09 17:14:47 -0500

    Set up for enemy cubes

--- p1.js
+++ p1.js
@@ -1,18 +1,37 @@
+// Necessary parameters
 let plane = "floor";
 let yCubeMovement = 0;
 let yCubeTarget = 0;
+randomYHeight = Math.floor(Math.random() * -489);
 
+// Initialize application
 let app = new PIXI.Application({width: 1280, height: 720, backgroundColor: 0x403E3E});
 document.body.appendChild(app.view);
 
+let rogueCube = new PIXI.Graphics();
+rogueCube.beginFill(0x000000);
+rogueCube.drawRect(1280, 564, 81, 81);
+rogueCube.endFill();
+app.stage.addChild(rogueCube);
+
+
+function sendRogueCube(){
+	rogueCube.setTransform(rogueCube.x - 5, randomYHeight);
+}
+
+
+
+// Create ceiling, floor, and player cube objects
 let ceiling = new PIXI.Graphics();
 let floor = new PIXI.Graphics();
-
+// Cube is a container to make cube fill and outline
 const cube = new PIXI.Container();
 
+// Cube color fill and outline
 let cubeFill = new PIXI.Graphics();
 let cubeOutline = new PIXI.Graphics();
 
+// Define all colors and shape sizes
 ceiling.beginFill(0x0B7CDF);
 ceiling.drawRect(0, 0, 1280, 75);
 ceiling.endFill();
@@ -29,27 +48,51 @@ cubeOutline.beginFill(0x000000);
 cubeOutline.drawRect(10, 720-75-80, 81, 81);
 cubeOutline.endFill();
 
-
+// Create cube with fill and outline
 cube.addChild(cubeOutline);
 cube.addChild(cubeFill);
 
-
+// Add elements to stage
 app.stage.addChild(ceiling);
 app.stage.addChild(floor);
 app.stage.addChild(cube);
 
+//
+function checkCollision(){
+	rogueCubeMinX = rogueCube.getBounds().x;
+	rogueCubeMaxX = rogueCube.getBounds().x + rogueCube.getBounds().width;
+	rogueCubeMinY = rogueCube.getBounds().y;
+	rogueCubeMaxY = rogueCube.getBounds().y + rogueCube.getBounds().height;
 
+	cubeMinX = cube.getBounds().x;
+	cubeMaxX = cube.getBounds().x + cube.getBounds().width;
+	cubeMinY = cube.getBounds().y;
+	cubeMaxY = cube.getBounds().y + cube.getBounds().height;
+
+	// TODO: Check if cubes are touching
+	if( false ){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+// Loop
  app.ticker.add((delta) =>{
 	 if(cube.y != yCubeTarget){
 	 		cube.setTransform(cube.x, cube.y + yCubeMovement);
 		}
+		sendRogueCube();
+		checkCollision();
+
  });
 
-
+// Handle button presses
 function buttonHandler(event){
-
 	let key = event.key;
 	console.log(key)
+
 	if(key == " "){
 		if(plane == "floor"){
 			plane = "ceiling";
